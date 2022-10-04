a3da977c622e9158cb252389c7eceffdb1095844
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2022-10-04 23:10:13 +0400

    links

--- example/admin.py
+++ example/admin.py
@@ -3,12 +3,27 @@ from django.contrib import admin
 from django_admin_filters import DateRange, DateRangePicker
 from .models import Log
 
+
+class MultiChoiceFilter(admin.filters.ChoicesFieldListFilter):
+    """Multiselect options filter.
+
+    https://groups.google.com/g/django-users
+    https://stackoverflow.com/questions/39790087/is-multi-choice-django-admin-filters-possible
+
+    https://github.com/carltongibson/django-filter
+
+    https://github.com/ctxis/django-admin-multiple-choice-list-filter
+    https://github.com/modlinltd/django-advanced-filters
+    https://stackoverflow.com/questions/38508672/django-admin-filter-multiple-select
+    """
+
+
 class Admin(admin.ModelAdmin):
     """Admin site customization."""
 
     list_display = ['text', 'status', 'timestamp1', 'timestamp2']
     list_filter = (
-      ('status', admin.filters.ChoicesFieldListFilter),
+      ('status', MultiChoiceFilter),
       ('timestamp1', DateRange),
       ('timestamp2', DateRangePicker)
     )
