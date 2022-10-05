5b2dc1535a65b88e81ed6260d469b5c733bc5f42
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2022-10-05 10:27:18 +0400

    fix example

--- example/admin.py
+++ example/admin.py
@@ -4,14 +4,32 @@ from django_admin_filters import DateRange, DateRangePicker, MultiChoice
 from .models import Log
 
 
+class StatusFilter(MultiChoice):
+    """Field status filter."""
+
+    FILTER_LABEL = "By status"
+
+
+class Timestamp1Filter(DateRange):
+    """Field timestamp1 filter."""
+
+    FILTER_LABEL = "By timestamp1"
+
+
+class Timestamp2Filter(DateRangePicker):
+    """Field timestamp2 filter."""
+
+    FILTER_LABEL = "By timestamp2"
+
+
 class Admin(admin.ModelAdmin):
     """Admin site customization."""
 
     list_display = ['text', 'status', 'timestamp1', 'timestamp2']
     list_filter = (
-      ('status', MultiChoice),
-      ('timestamp1', DateRange),
-      ('timestamp2', DateRangePicker)
+      ('status', StatusFilter),
+      ('timestamp1', Timestamp1Filter),
+      ('timestamp2', Timestamp2Filter)
     )
 
 
