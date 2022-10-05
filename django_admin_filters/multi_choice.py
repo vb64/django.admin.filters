913d3599297356f47c5c93fbb04a7c8a2517336d
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2022-10-05 09:25:08 +0400

    step

---
+++ django_admin_filters/multi_choice.py
@@ -0,0 +1,16 @@
+"""Django admin multi choice filter with checkboxes for db fields with choices option."""
+from django.contrib import admin
+
+
+class MultiChoice(admin.filters.ChoicesFieldListFilter):
+    """Multiselect options filter.
+
+    https://groups.google.com/g/django-users
+    https://stackoverflow.com/questions/39790087/is-multi-choice-django-admin-filters-possible
+
+    https://github.com/carltongibson/django-filter
+
+    https://github.com/ctxis/django-admin-multiple-choice-list-filter
+    https://github.com/modlinltd/django-advanced-filters
+    https://stackoverflow.com/questions/38508672/django-admin-filter-multiple-select
+    """
--- example/admin.py
+++ example/admin.py
@@ -1,21 +1,7 @@
 """Admin site."""
 from django.contrib import admin
-from django_admin_filters import DateRange, DateRangePicker
+from django_admin_filters import DateRange, DateRangePicker, MultiChoice
 from .models import Log
-
-
-class MultiChoiceFilter(admin.filters.ChoicesFieldListFilter):
-    """Multiselect options filter.
-
-    https://groups.google.com/g/django-users
-    https://stackoverflow.com/questions/39790087/is-multi-choice-django-admin-filters-possible
-
-    https://github.com/carltongibson/django-filter
-
-    https://github.com/ctxis/django-admin-multiple-choice-list-filter
-    https://github.com/modlinltd/django-advanced-filters
-    https://stackoverflow.com/questions/38508672/django-admin-filter-multiple-select
-    """
 
 
 class Admin(admin.ModelAdmin):
@@ -23,7 +9,7 @@ class Admin(admin.ModelAdmin):
 
     list_display = ['text', 'status', 'timestamp1', 'timestamp2']
     list_filter = (
-      ('status', MultiChoiceFilter),
+      ('status', MultiChoice),
       ('timestamp1', DateRange),
       ('timestamp2', DateRangePicker)
     )
