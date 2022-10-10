b6a07fac24e52e24230b338c4bb1ba780b35f3a3
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2022-10-10 11:27:29 +0400

    Separate Choices filter functionality

--- django_admin_filters/multi_choice.py
+++ django_admin_filters/multi_choice.py
@@ -2,7 +2,39 @@
 from .base import Filter as BaseFilter
 
 
-class Filter(BaseFilter):
+class Choices:
+    """Multi choice options filter."""
+
+    template = 'multi_choice.html'
+    selected = []
+    lookup_choices = []
+
+    FILTER_LABEL = "By choices"
+    CHOICES_SEPARATOR = ','
+
+    def set_selected(self, val, title):
+        """Init choices according request parameter string."""
+        title.update({
+          'choices_separator': self.CHOICES_SEPARATOR,
+        })
+        self.selected = val.split(self.CHOICES_SEPARATOR) if val else []
+        self.lookup_choices = self.get_lookup_choices()
+
+    def get_lookup_choices(self):
+        """Return filter choices."""
+        return []
+
+    def choices(self, _changelist):
+        """Define filter checkboxes."""
+        for lookup, title in self.lookup_choices:
+            yield {
+              'selected': lookup in self.selected,
+              'value': lookup,
+              'display': title,
+            }
+
+
+class Filter(BaseFilter, Choices):
     """Multi choice options filter.
 
     For CharField and IntegerField fields with 'choices' option.
@@ -13,37 +45,22 @@ class Filter(BaseFilter):
     https://github.com/modlinltd/django-advanced-filters
     """
 
-    template = 'multi_choice.html'
     parameter_name_mask = 'mchoice_'
-
-    FILTER_LABEL = "By choices"
-    CHOICES_SEPARATOR = ','
 
     def __init__(self, field, request, params, model, model_admin, field_path):
         """Extend base functionality."""
         super().__init__(field, request, params, model, model_admin, field_path)
+        self.set_selected(self.value(), self.title)
 
-        self.title.update({
-          'choices_separator': self.CHOICES_SEPARATOR,
-        })
-        val = self.value()
-        self.selected = val.split(self.CHOICES_SEPARATOR) if val else []
-        self.lookup_choices = self.get_lookup_choices()
+    def choices(self, changelist):
+        """Call shared implementation."""
+        return Choices.choices(self, changelist)
 
     def get_lookup_choices(self):
         """Return filter choices."""
         if self.field.get_internal_type() in ['IntegerField']:
             self.selected = [int(i) for i in self.selected]
         return self.field.flatchoices
-
-    def choices(self, changelist):
-        """Define filter checkboxes."""
-        for lookup, title in self.lookup_choices:
-            yield {
-              'selected': lookup in self.selected,
-              'value': lookup,
-              'display': title,
-            }
 
     def queryset(self, request, queryset):
         """Return the filtered by selected options queryset."""
@@ -57,7 +74,7 @@ class Filter(BaseFilter):
 
 
 class FilterExt(Filter):
-    """Extended variant of previous filter, that allows filtering by custom defined properties."""
+    """Allows filtering by custom defined properties."""
 
     options = []
 
